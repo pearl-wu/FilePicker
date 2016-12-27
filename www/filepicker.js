5978a61d01dcf04e3a8bc625837cec561332d98d
pearl <lu92500@yahoo.com.tw>
2016-12-27 10:29:49 +0800

    Update filepicker.js

--- www/filepicker.js
+++ www/filepicker.js
@@ -4,6 +4,9 @@ exports.start = function(options) {
     if(typeof options == "undefined"){
         options = {};
     }
-
     exec(function(){}, function(){}, "filepicker", "start", [options]);
 };
+
+exports.get = function(message, win, fail){
+    exec(win, fail, "filepicker", "get", [message]);
+}
