349f8353816e02c37eb1bc2a566baccaad3715f3
pearl <lu92500@yahoo.com.tw>
2016-12-30 12:01:08 +0800

    Update filepicker.java

--- src/android/filepicker.java
+++ src/android/filepicker.java
@@ -93,6 +93,27 @@ public class filepicker extends CordovaPlugin {
     	  return true;
        } 
        
+       if(action.equals("openfile")){
+    	   JSONObject params = args.getJSONObject(0);
+    	   String webUrl = params.getString("url");
+    	   
+    	   PluginResult.Status status = PluginResult.Status.OK;
+           String result = "";
+
+           try{
+               if(action.equals("openFile")) {
+                   openFile(args.getString(0));
+               }else{
+                   status = PluginResult.Status.INVALID_ACTION;
+               }
+               return true;
+           }catch(JSONException e){
+               return false;
+           }catch(IOException e){
+               return false;
+           }
+       } 
+       
         return false;
     }
 
@@ -181,6 +202,78 @@ public class filepicker extends CordovaPlugin {
 		   return false;
 	  }
  }
+   
+   @SuppressWarnings("unused")
+private void openFile(String url) throws IOException {
+       // Create URI
+       Uri uri = Uri.parse(url);
+       Intent intent = null;
+       // Check what kind of file you are trying to open, by comparing the url with extensions.
+       // When the if condition is matched, plugin sets the correct intent (mime) type,
+       // so Android knew what application to use to open the file
+
+       if (url.contains(".doc") || url.contains(".docx")) {
+           // Word document
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "application/msword");
+       } else if(url.contains(".pdf")) {
+           // PDF file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "application/pdf");
+       } else if(url.contains(".ppt") || url.contains(".pptx")) {
+           // Powerpoint file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+       } else if(url.contains(".xls") || url.contains(".xlsx")) {
+           // Excel file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "application/vnd.ms-excel");
+       } else if(url.contains(".rtf")) {
+           // RTF file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "application/rtf");
+       } else if(url.contains(".wav")) {
+           // WAV audio file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "audio/x-wav");
+       } else if(url.contains(".gif")) {
+           // GIF file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "image/gif");
+       } else if(url.contains(".jpg") || url.contains(".jpeg")) {
+           // JPG file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "image/jpeg");
+       } else if(url.contains(".png")) {
+           // PNG file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "image/png");
+       } else if(url.contains(".txt")) {
+           // Text file
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "text/plain");
+       } else if(url.contains(".mpg") || url.contains(".mpeg") || url.contains(".mpe") || url.contains(".mp4") || url.contains(".avi")) {
+           // Video files
+           intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "video/*");
+       }
+
+       //if you want you can also define the intent type for any other file
+
+       //additionally use else clause below, to manage other unknown extensions
+       //in this case, Android will show all applications installed on the device
+       //so you can choose which application to use
+
+
+       else {            
+       	intent = new Intent(Intent.ACTION_VIEW);
+           intent.setDataAndType(uri, "*/*");
+       }
+
+       this.cordova.getActivity().startActivity(intent);
+   } 
+   
+   
  
  private void showToast(final String message, final String duration) {
   cordova.getActivity().runOnUiThread(new Runnable() {
