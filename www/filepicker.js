58f2812504388eadfc2f8850e11ddfd928d3830c
pearl <lu92500@yahoo.com.tw>
2017-01-18 15:04:40 +0800

    Update filepicker.js

--- www/filepicker.js
+++ www/filepicker.js
@@ -35,7 +35,7 @@ exports.choosepicture = function(win, fail, options) {
         titleText: options.titleText ? options.titleText : "Gallery",
         errorMessageText: options.errorMessageText ? options.errorMessageText : "Max limit reached!"
     };
-    return cordova.exec(win, fail, "filepicker", "choosepicture", [params]);
+    exec(win, fail, "filepicker", "choosepicture", [params]);
 };
 
 exports.chooesfiile = function(message, win, fail){
