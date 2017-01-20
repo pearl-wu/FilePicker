b662fad55ed3814ae95b73b3869bf5fb6e912b3b
pearl <lu92500@yahoo.com.tw>
2017-01-20 14:47:44 +0800

    Update CustomfileActivity.java

--- src/android/CustomfileActivity.java
+++ src/android/CustomfileActivity.java
@@ -6,42 +6,44 @@ import java.util.ArrayList;
 import com.orleonsoft.android.simplefilechooser.Constants;
 
 import am.armsoft.data.Category;
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 
-public class CustomfileActivity extends ListActivity implements OnItemClickListener {
+public class CustomfileActivity extends Activity implements OnItemClickListener {
 	
 	private File currentFolder;
 	private ArrayList<String> extensions;
-	Context _context;
 	Category currentCategory;
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
-		String PACKAGE_NAME = getPackageName();
-		int main = getResources().getIdentifier("multiselectorfile", "layout",  PACKAGE_NAME);
+		int main = getResources().getIdentifier("multiselectorfile", "layout",  getPackageName());
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(main);
 				
-		/*Bundle extras = getIntent().getExtras();	
+		Bundle extras = getIntent().getExtras();	
 		if (extras != null) {
 			if (extras.getStringArrayList(Constants.KEY_FILTER_FILES_EXTENSIONS) != null) {
 				extensions = extras.getStringArrayList(Constants.KEY_FILTER_FILES_EXTENSIONS);
-				Toast.makeText(_context, extensions.toString(), Toast.LENGTH_LONG).show();
-				FileUtils.getSpecificTypeOfFile(_context, new String[]{extensions.toString()});
+				//Toast.makeText(this, extensions.toString(), Toast.LENGTH_LONG).show();
+				FileUtils.getSpecificTypeOfFile(this, new String[]{extensions.toString()});
 			}			
 		}
 		
 		currentFolder = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
 		currentCategory = new Category();
-		currentCategory.path = currentFolder.getAbsolutePath(); */		
+		currentCategory.path = currentFolder.getAbsolutePath();		
 			
 	}
 
