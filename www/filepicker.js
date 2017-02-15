db5edc7ff720ae9120eeaf2c181d0f4c34912ea9
pearl <lu92500@yahoo.com.tw>
2017-02-15 17:22:22 +0800

    Add files via upload

--- www/filepicker.js
+++ www/filepicker.js
@@ -7,15 +7,12 @@ exports.start = function(options) {
     exec(function(){}, function(){}, "filepicker", "start", [options]);
 };
 
-exports.start = function(options) {
-    if(typeof options == "undefined"){
-        options = {};
-    }
-    exec(function(){}, function(){}, "filepicker", "start", [options]);
-};
-
 exports.version = function(win, fail){
     exec(win, fail, "filepicker", "version", []);
+}
+
+exports.get = function(message, win, fail){
+    exec(win, fail, "filepicker", "get", [message]);
 }
 
 exports.find = function(message, win, fail){
