3b50ef616c736cf150742e681d828f39d1c8deec
pearl <lu92500@yahoo.com.tw>
2017-06-12 16:17:26 +0800

    Update CustomfileActivity.java

--- src/android/CustomfileActivity.java
+++ src/android/CustomfileActivity.java
@@ -1,21 +1,14 @@
 package com.bais.filepicker;
 
-import java.io.File;
-import java.security.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
+import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -25,7 +18,12 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class CustomfileActivity extends Activity{
@@ -37,116 +35,119 @@ public class CustomfileActivity extends Activity{
 	private long[] sz;
 	GridView gridGallery;
 	GalleryAdapter adapter;
+	private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+	private static final int REQUEST_CONTACTS = 1;
 
+	@RequiresApi(api = Build.VERSION_CODES.N)
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	    int main = this.getResources().getIdentifier("multiselectorfile", "layout", this.getPackageName());
-		setContentView(main);		
-				
+		setContentView(main);
+		nameinfo();
+	}
+
+
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	public void nameinfo(){
 		Bundle extras = getIntent().getExtras();
-		extensions = extras.getStringArrayList(Constants.KEY_FILTER_FILES_EXTENSIONS);	
+		extensions = extras.getStringArrayList(Constants.KEY_FILTER_FILES_EXTENSIONS);
 
 		if (extras != null) {
 			if (extras.getStringArrayList(Constants.KEY_FILTER_FILES_EXTENSIONS) != null) {
+				String[] projection = new String[]{
+						MediaStore.Files.FileColumns.TITLE,
+						MediaStore.Files.FileColumns.DATA,
+						MediaStore.Files.FileColumns.DATE_ADDED,
+						MediaStore.Files.FileColumns.SIZE,
+						MediaStore.Files.FileColumns.DATE_MODIFIED
+				};
+				Uri uri = MediaStore.Files.getContentUri("external");
+				String selection = "";
+				for(int i=0;i<extensions.size();i++)
+				{
+					if(i!=0)
+					{
+						selection = selection+" OR ";
+					}
+					selection = selection+MediaStore.Files.FileColumns.DATA+" LIKE '%"+extensions.get(i)+"' ";
+				}
+				String sortOrder = MediaStore.Files.FileColumns.DATE_MODIFIED + " DESC";
+				Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, selection, null, sortOrder);
+				List<HashMap<String, Object>> fillMaps;
 
-		        String[] projection = new String[]{  
-		        	MediaStore.Files.FileColumns.TITLE,
-		        	MediaStore.Files.FileColumns.DATA,
-		        	MediaStore.Files.FileColumns.DATE_ADDED,
-		        	MediaStore.Files.FileColumns.SIZE,
-		        	MediaStore.Files.FileColumns.DATE_MODIFIED
-		        };
-		        Uri uri = MediaStore.Files.getContentUri("external"); 
-		        String selection = "";  
-		        for(int i=0;i<extensions.size();i++)  
-		        {  
-		            if(i!=0)  
-		            {  
-		                selection = selection+" OR ";  
-		            }  
-		            selection = selection+MediaStore.Files.FileColumns.DATA+" LIKE '%"+extensions.get(i)+"' ";
-		        }  
-		        String sortOrder = MediaStore.Files.FileColumns.DATE_MODIFIED + " DESC";  
-		        ContentResolver resolver = this.getContentResolver();  
-		        Cursor cursor = resolver.query(uri, projection, selection, null, sortOrder);  
-		        
-		        
-		       List<HashMap<String, Object>> fillMaps;
-		       
-		        url = new String[cursor.getCount()];
-		        sz = new long[cursor.getCount()];
-		        fillMaps = new ArrayList<HashMap<String, Object>>();
-		        if(cursor == null) return;
-		        Log.e("檔案總數量","共 "+cursor.getCount()+" 個");
-		        if(cursor.moveToLast()){
-		        	for(int a=0; a<cursor.getCount(); a++){
-		        	   cursor.moveToPosition(a);
-		               String data = cursor.getString(1);
-		               String[] tokens = data.split("/");
-		               String filename = tokens[tokens.length-1];
-		               long sx = Long.valueOf(cursor.getString(3));
-		               long timestamp = Long.parseLong(cursor.getString(4));
-		               File files = new File(data);
-		               Calendar cal = Calendar.getInstance();
-		               long time = files.lastModified();
-		               SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-		               cal.setTimeInMillis(time);
-		               //Toast.makeText(getApplication(), formatter.format(cal.getTime()), Toast.LENGTH_SHORT).show();
-		               
-		               HashMap<String, Object> map = new HashMap<String, Object>();
-		               //Toast.makeText(this, String.valueOf(filename.substring(filename.lastIndexOf(".")+1).toLowerCase()), Toast.LENGTH_SHORT).show();
-		               String at = String.valueOf(filename.substring(filename.lastIndexOf(".")+1).toLowerCase());
-		               if(at=="7z") at = "z";
-		               int flags = this.getResources().getIdentifier(at, "drawable", this.getPackageName());
-		               map.put("filetapy", flags);
-		               map.put("filename", filename);
-		               map.put("filesize", FormaetfileSize(sx));
-		               map.put("filetime", formatter.format(cal.getTime()));
-		               fillMaps.add(map);
-		               sz[a] = sx;
-		               url[a] = data;     
-		              // Toast.makeText(this, sz[a]+"", Toast.LENGTH_SHORT).show();
-		            }
-		        	 cursor.close();
-		        	
-		    	    int main_item = this.getResources().getIdentifier("multiselectorfile_item", "layout", this.getPackageName());
-		    	    int lists = this.getResources().getIdentifier("list", "id", this.getPackageName());
-		    	    int icong = this.getResources().getIdentifier("icon", "id", this.getPackageName());
-		    	    int sline = this.getResources().getIdentifier("secondLine", "id", this.getPackageName());
-		    	    int time = this.getResources().getIdentifier("timeLine", "id", this.getPackageName());
-		    	    int fline = this.getResources().getIdentifier("firstLine", "id", this.getPackageName());
-		        	String[] from = new String[] { "filetapy", "filename", "filesize", "filetime"};
-		            int[] to = new int[] {icong, fline, sline, time };
-		            listView = (ListView)findViewById(lists);
-		            SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), fillMaps, main_item, from, to);
-		            listView.setAdapter(adapter);			            
-		           		
-		    		listView.setOnItemClickListener(new OnItemClickListener(){
-		                 public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-		                 {            	 
-		                     //Toast.makeText(CustomfileActivity.this, "您點選了第 "+sz[position], Toast.LENGTH_SHORT).show();
-		                	 Intent data = new Intent();
-		                	 data.putExtra("single_path", url[position]);
-		                	 data.putExtra("sz", sz[position]);
-		                	 try{
-		         				setResult(RESULT_OK, data);
-		    	     		 } catch (Exception e){
-		    	     				e.printStackTrace();
-		    	     		 }
-		    	     		 finish();
-		                 }
-		            }
-		           );
-		        }
-			}		
+				url = new String[cursor.getCount()];
+				sz = new long[cursor.getCount()];
+				fillMaps = new ArrayList<HashMap<String, Object>>();
+				if(cursor == null) return;
+				Log.e("檔案總數量","共 "+cursor.getCount()+" 個");
+				if(cursor.moveToLast()){
+					for(int a=0; a<cursor.getCount(); a++){
+						cursor.moveToPosition(a);
+						String data = cursor.getString(1);
+						String[] tokens = data.split("/");
+						String filename = tokens[tokens.length-1];
+						long sx = Long.valueOf(cursor.getString(3));
+						long timestamp = Long.parseLong(cursor.getString(4));
+						File files = new File(data);
+						Calendar cal = Calendar.getInstance();
+						long time = files.lastModified();
+						SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+						cal.setTimeInMillis(time);
+						//Toast.makeText(getApplication(), formatter.format(cal.getTime()), Toast.LENGTH_SHORT).show();
+
+						HashMap<String, Object> map = new HashMap<String, Object>();
+						//Toast.makeText(this, String.valueOf(filename.substring(filename.lastIndexOf(".")+1).toLowerCase()), Toast.LENGTH_SHORT).show();
+						String at = String.valueOf(filename.substring(filename.lastIndexOf(".")+1).toLowerCase());
+						if(at=="7z") at = "z";
+						int flags = this.getResources().getIdentifier(at, "drawable", this.getPackageName());
+						map.put("filetapy", flags);
+						map.put("filename", filename);
+						map.put("filesize", FormaetfileSize(sx));
+						map.put("filetime", formatter.format(cal.getTime()));
+						fillMaps.add(map);
+						sz[a] = sx;
+						url[a] = data;
+						// Toast.makeText(this, sz[a]+"", Toast.LENGTH_SHORT).show();
+					}
+					cursor.close();
+
+					int main_item = this.getResources().getIdentifier("multiselectorfile_item", "layout", this.getPackageName());
+					int lists = this.getResources().getIdentifier("list", "id", this.getPackageName());
+					int icong = this.getResources().getIdentifier("icon", "id", this.getPackageName());
+					int sline = this.getResources().getIdentifier("secondLine", "id", this.getPackageName());
+					int time = this.getResources().getIdentifier("timeLine", "id", this.getPackageName());
+					int fline = this.getResources().getIdentifier("firstLine", "id", this.getPackageName());
+					String[] from = new String[] { "filetapy", "filename", "filesize", "filetime"};
+					int[] to = new int[] {icong, fline, sline, time };
+					listView = (ListView)findViewById(lists);
+					SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), fillMaps, main_item, from, to);
+					listView.setAdapter(adapter);
+
+					listView.setOnItemClickListener(new OnItemClickListener(){
+						public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+						{
+							//Toast.makeText(CustomfileActivity.this, "您點選了第 "+sz[position], Toast.LENGTH_SHORT).show();
+							Intent data = new Intent();
+							data.putExtra("single_path", url[position]);
+							data.putExtra("sz", sz[position]);
+							try{
+								setResult(RESULT_OK, data);
+							} catch (Exception e){
+								e.printStackTrace();
+							}
+							finish();
+						}
+					});
+				}
+			}
 		}
-	}	
+
+	}
 
 
-	public static String FormaetfileSize(long size){		
+	public static String FormaetfileSize(long size){
 		 if (size <= 0)
 		      return "0";
 		    final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
