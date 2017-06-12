61d6fd8bdbbac12939c44e7f1a9f4cafe3a7fd1a
pearl <lu92500@yahoo.com.tw>
2017-06-12 16:53:46 +0800

    Add files via upload

---
+++ src/android/selfperminnion.java
@@ -0,0 +1,40 @@
+package com.bais.filepicker;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.PermissionChecker;
+
+/**
+ * Created by pearl on 2017/6/12.
+ */
+
+public class selfperminnion {
+    public boolean selfPermissionGranted;
+
+    public boolean selfPermissionGranted(Context context, String permission) {
+        // Android 6.0 以前，默認
+        boolean result = true;
+        int targetSdkVersion = 16;
+        
+        try {
+            final PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            targetSdkVersion = info.applicationInfo.targetSdkVersion;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (targetSdkVersion >= Build.VERSION_CODES.M) {
+                // targetSdkVersion >= 23, 使用Context#checkSelfPermission
+                result = context.checkSelfPermission(permission)  == PackageManager.PERMISSION_GRANTED;
+            } else {
+                // targetSdkVersion < 23, 需要使用 PermissionChecker
+                result = PermissionChecker.checkSelfPermission(context, permission) == PermissionChecker.PERMISSION_GRANTED;
+            }
+        }
+        return result;
+    }
+
+}
